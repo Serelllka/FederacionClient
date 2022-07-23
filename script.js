d5c4262358483557df3719f36e3bac9fd54613ae
Serelllka <vprogamervip@gmail.com>
2022-07-23 20:23:07 +0500

    adds script file

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-document.getElementById('myElement').onclick = function() {
-    var xhr = new XMLHttpRequest()
-    xhr.open('POST', 'http://localhost:8000/auth/sign-in', false)
-    xhr.send('{ "username": "lol3", "password": "lol3" }')
-    if (xhr.status != 200) {
-        alert( xhr.status + ': ' + xhr.statusText );
-      } else {
-        alert( xhr.responseText );
-      }
+// document.getElementById('sign__in').onclick = function() {
+//   var xhr = new XMLHttpRequest()
+//   xhr.open('POST', 'http://localhost:8000/auth/sign-in', false)
+//   xhr.send('{ "username": "lol3", "password": "lol3" }')
+//   if (xhr.status != 200) {
+//     alert( xhr.status + ': ' + xhr.statusText );
+//   } else {
+//     alert( xhr.responseText );
+//   }
+// }
+
+function signInHandler() {
+  swapDiv('sign__in', 'sign__up');
+}
+
+function signUpHandler() {
+  swapDiv('sign__up', 'sign__in');
+}
+
+document.getElementById('sign__in').onclick = signInHandler;
+
+document.getElementById('sign__up').onclick = signUpHandler;
+
+function swapDiv(firstElemId, secondElemId) {
+  var firstElemContent = document.getElementById(firstElemId);
+  var secondElemContent = document.getElementById(secondElemId);
+  firstElemContent.parentNode.insertBefore(firstElemContent, secondElemContent);
+
+  firstElemContent.classList.add('btn');
+  firstElemContent.classList.remove('intro__auth__form__item__grey');
+
+  secondElemContent.classList.remove('btn');
+  secondElemContent.classList.add('intro__auth__form__item__grey');
 }
