9ae9c0d46b739b7b72d1a6a275ace2473e22c29f
Serelllka <vprogamervip@gmail.com>
2022-07-27 00:00:18 +0500

    adds some features

--- assets/scritps/main.js
+++ assets/scritps/main.js
@@ -3,103 +3,29 @@ const config = {
     baseUrl: 'http://localhost:8000'
 }
 
-var cast = {
-	"characters": [
-		{
-			"name": "Jon Snow",
-			"shortCode": "jon-snow",
-			"actor": "Kit Harrington",
-			"house": "Stark",
-			"location": "The Wall"
-		},
-		{
-			"name": "Tyrion Lannister",
-			"shortCode": "tyrion",
-			"actor": "Peter Dinklage",
-			"house": "Lannister"
-		},
-		{
-			"name": "Brienne of Tarth",
-			"shortCode": "brienne",
-			"actor": "Gwendoline Christie",
-			"house": "Tarth",
-			"location": "In search of Sansa Stark"
-		}
-	]
-}
-
-var cast = {
-	"characters": [
-		{
-			"name": "Jon Snow",
-			"shortCode": "jon-snow",
-			"actor": "Kit Harrington",
-			"house": "Stark",
-			"location": "The Wall"
-		},
-		{
-			"name": "Tyrion Lannister",
-			"shortCode": "tyrion",
-			"actor": "Peter Dinklage",
-			"house": "Lannister"
-		},
-		{
-			"name": "Brienne of Tarth",
-			"shortCode": "brienne",
-			"actor": "Gwendoline Christie",
-			"house": "Tarth",
-			"location": "In search of Sansa Stark"
-		},
-		{
-			"name": "Eddard Stark",
-			"shortCode": "ned-stark",
-			"actor": "Sean Bean",
-			"house": "Stark",
-			"location": "Winterfell"
-		},
-		{
-			"name": "Sandor Clegane",
-			"shortCode": "the-hound",
-			"actor": "Rory McCann",
-			"house": "Clegane",
-			"location": "unknown"
-		}
-	]
-}
-
 $(document).ready(function() {
-    var client = new UserClient(config.baseUrl);
-    const promise1 = client.getAllUsers().then(value => {
-		console.log(value)
-        var characterTemplate = $("#condemner__template").html();
-
-        var compiledCharacterTemplate = Handlebars.compile(characterTemplate);
-
-        $(".condemner__list__container").html(compiledCharacterTemplate(value));
-    });
-
     var client = new UserClient(config.baseUrl);
 	const promise2 = client.login('string', 'string').then(item => {
 		client.getAllArticles(item.token).then(value => {
 			console.log(value)
-			var characterTemplate = $("#convicted__template").html();
+			var characterTemplate = $("#article__template").html();
 	
 			var compiledCharacterTemplate = Handlebars.compile(characterTemplate);
 	
-			$(".convicted__list__container").html(compiledCharacterTemplate(value));
+			$(".article__list__container").html(compiledCharacterTemplate(value));
 		})
 	});
+
+    const promise1 = client.getAllUsers().then(value => {
+		console.log(value)
+        var characterTemplate = $("#convicted__template").html();
+
+        var compiledCharacterTemplate = Handlebars.compile(characterTemplate);
+
+        $(".convicted__list__container").html(compiledCharacterTemplate(value));
+    });
 
 	Promise.all([promise1, promise2]).then(_ => {
 		initDataList()
 	})
 });
-
-$(document).ready(function() {
-	var client = new UserClient(config.baseUrl);
-		client.jopa({
-			name: -1,
-			username: -2,
-			password: -3
-		})
-})
